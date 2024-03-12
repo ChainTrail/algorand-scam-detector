f4683a2ddac6864320dde130be4828c881e8e8ed
ChainTrail <service@chaintrail.io>
2024-03-12 10:03:36 +0800

    Added CORS headers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import { OpenAPIRouter } from "@cloudflare/itty-router-openapi";
 import { ScamDetectorFetch } from "./endpoints/scamDetectorFetch";
+import { createCors } from 'itty-router'
 
 export const router = OpenAPIRouter({
 	docs_url: "/",
 });
+const { preflight, corsify } = createCors({
+	methods: ['GET'],
+  	origins: ["-"], // TODO: research why * is not working here. Needs the custom header object to configure ACAO correctly.
+	headers: {
+		"Access-Control-Allow-Origin": "*"
+	}
+});
 
+// embed preflight upstream to handle all OPTIONS requests
+router.all('*', preflight);
+
+// Scam endpoint
 router.get("/api/scam/:txId/", ScamDetectorFetch);
 
 // 404 for everything else
@@ -19,5 +31,7 @@ router.all("*", () =>
 );
 
 export default {
-	fetch: router.handle,
+	fetch: async (request, env, ctx) => {
+		return router.handle(request, env, ctx).then(corsify)
+	 },
 };
